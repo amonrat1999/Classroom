1fa73c22382aca4833c485c567dfdefcb5f8e2a4
amonrat1999 <1704amonrat@gmail.com>
2021-08-24 14:23:10 +0700

    add angular materail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 //Angular Materail
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIconModule } from '@angular/material/icon';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +17,7 @@ import { StudentComponent } from './Components/student/student.component';
 import { TeacherComponent } from './Components/teacher/teacher.component';
 import { ClassroomComponent } from './Components/classroom/classroom.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+
 
 
 
@@ -40,7 +44,8 @@ import { NavbarComponent } from './Components/navbar/navbar.component';
     MatProgressSpinnerModule,
     MatIconModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatPaginatorModule
 
   ],
   providers: [],
